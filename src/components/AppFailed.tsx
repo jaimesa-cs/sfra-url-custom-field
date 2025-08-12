84465e9396311194c84e2ee8f7bb8bfe5497a71b
Jaime Santos Alcon <jaime.santosalcon@contentstack.com>
2025-08-12 12:13:50 -0400

    Updated depdencies, fixed build

--- src/components/AppFailed.tsx
+++ src/components/AppFailed.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import localeTexts from "../common/locales/en-us";
 
 /**
@@ -17,7 +17,8 @@ export const AppFailed = () => {
             <path
               fillRule="evenodd"
               d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-              clipRule="evenodd"></path>
+              clipRule="evenodd"
+            ></path>
           </svg>
           <h3>
             {localeTexts.AppFailed.Message1} <br />
@@ -32,7 +33,8 @@ export const AppFailed = () => {
               <path
                 fillRule="evenodd"
                 d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                clipRule="evenodd"></path>
+                clipRule="evenodd"
+              ></path>
             </svg>
             {localeTexts.AppFailed.button.text}
           </button>
